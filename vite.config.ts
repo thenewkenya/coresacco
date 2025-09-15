2d674edf5b5e407896855b77ccd29eb2a2521fd8
Daniel Kosgei <dannykosgei@gmail.com>
2025-09-15 06:55:39 +0300

    deployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
@@ -13,9 +12,6 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
     ],
     esbuild: {
         jsx: 'automatic',
